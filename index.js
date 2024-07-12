d43b3c7f5fb654cd713ff37392d3aa46e36b5edb
Geoff Priestner <Geoff@DN-UK.COM>
2024-07-12 12:16:51 +0100

    Back face culling

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ addEventListener("resize", resize);
 // view.moveTo(0, 0);
 // view.lineTo(100, 100);
 // view.stroke();
+function dotProcuct(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+function crossProduct(v1, v2) {
+    const cp = { x: v1.y * v2.z - v1.z * v2.y, y: v1.z * v2.x - v1.x * v2.z, z: v1.x * v2.y - v1.y * v2.x };
+    const nv = normalizeVector(cp);
+    return nv;
+}
+function normalizeVector(v) {
+    const length = Math.sqrt(v.x **2 + v.y ** 2 + v.z **2);
+    return { x: v.x / length, y: v.y / length, z: v.z /length };
+}
+function subtractVector(v1, v2) { // return v1 -> v2
+    return { x: v2.x-v1.x, y: v2.y-v1.y, z: v2.z-v1.z };
+}
 
 class Pt {
     constructor(x, y, z) {
@@ -28,7 +43,38 @@ class Pt {
         this.z = z;
     }
 }
+class Face {
+    i = [];
+    constructor() { for (const i of arguments) { this.i.push(i); } }
+    draw(wPoints, xYpoints) {
+        const faceWorldPoints = [];
+        for (const i of this.i) faceWorldPoints.push(wPoints[i]);
+        const faceXyPoints = [];
+        for (const i of this.i) faceXyPoints.push(xYpoints[i]);
 
+        // calculate 2 face vectors
+        const vectorAB = subtractVector(faceWorldPoints[1], faceWorldPoints[0]);
+        const vectorAC = subtractVector(faceWorldPoints[2], faceWorldPoints[0]);
+
+        const normalVector = crossProduct(vectorAB, vectorAC);
+        const cameraVector = faceWorldPoints[0];
+        const normalizedCameraVector = normalizeVector(cameraVector);
+        const dp = dotProcuct(normalVector, normalizedCameraVector);
+        const visible = dp < 0;
+        
+        if(visible) {
+            view.beginPath();
+            this.moveTo(faceXyPoints[0]);
+            this.lineTo(faceXyPoints[1]);
+            this.lineTo(faceXyPoints[2]);
+            this.lineTo(faceXyPoints[3]);
+            this.lineTo(faceXyPoints[0]);
+            view.stroke();
+        }
+    }
+    moveTo(p) { view.moveTo(p.x, p.y); }
+    lineTo(p) { view.lineTo(p.x, p.y); }
+}
 class Cube {
     model = [
         new Pt(-1, 1, -1), 
@@ -40,42 +86,53 @@ class Cube {
         new Pt(1, -1, 1), 
         new Pt(-1, -1, 1)
     ]
+    faces = [
+        new Face(0,1,2,3), // front
+        new Face(5,4,7,6), // back
+        new Face(4,5,1,0), // top
+        new Face(3,2,6,7), // bottom
+        new Face(4,0,3,7), // left
+        new Face(1,5,6,2), // right
+    ]
     constructor(x, y, z, s = 1) {
         this.scale = s;
         this.position = { x, y, z };
         this.rotation = { x: 0, y: 0, z: 0 };
     }
     draw() {
-        const points = [];
+        const points = [], wPoints = [], xYpoints = [];
         for (let i = 0; i < this.model.length; ++i) {
             const lp = this.toLocalPoint(this.model[i])
             const wp = this.toWorldPoint(lp);
+            wPoints.push(wp);
             const cp = this.toXyPoint(wp);
-            points.push(cp);
+            xYpoints.push(cp);
         }
 
-        view.beginPath();
-        this.moveTo(points[0]);
-        this.lineTo(points[1]);
-        this.lineTo(points[2]);
-        this.lineTo(points[3]);
-        this.lineTo(points[0]);
+        for (const f of this.faces) f.draw(wPoints, xYpoints);
 
-        this.moveTo(points[4]);
-        this.lineTo(points[5]);
-        this.lineTo(points[6]);
-        this.lineTo(points[7]);
-        this.lineTo(points[4]);
+        // view.beginPath();
+        // this.moveTo(points[0]);
+        // this.lineTo(points[1]);
+        // this.lineTo(points[2]);
+        // this.lineTo(points[3]);
+        // this.lineTo(points[0]);
 
-        this.moveTo(points[0]);
-        this.lineTo(points[4]);
-        this.moveTo(points[1]);
-        this.lineTo(points[5]);
-        this.moveTo(points[2]);
-        this.lineTo(points[6]);
-        this.moveTo(points[3]);
-        this.lineTo(points[7]);
-        view.stroke();
+        // this.moveTo(points[4]);
+        // this.lineTo(points[5]);
+        // this.lineTo(points[6]);
+        // this.lineTo(points[7]);
+        // this.lineTo(points[4]);
+
+        // this.moveTo(points[0]);
+        // this.lineTo(points[4]);
+        // this.moveTo(points[1]);
+        // this.lineTo(points[5]);
+        // this.moveTo(points[2]);
+        // this.lineTo(points[6]);
+        // this.moveTo(points[3]);
+        // this.lineTo(points[7]);
+        // view.stroke();
     }
     toLocalPoint(p) {
         const r1 = this.rotate(p, this.rotation, "x");
